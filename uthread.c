cb53fa2dfbda8e0b99dca1ff0ef3eb5ec9be43ff
DrBinary9 <62635098+DrBinary9@users.noreply.github.com>
2025-03-18 12:49:54 +0530

    Update uthread.c

--- uthread.c
+++ uthread.c
@@ -3,23 +3,23 @@
 int uthread_create(uthread_t *thread, void *(*start_routine)(void *), void *arg) {
     void *stack = malloc(STACK_SIZE);
     if (stack == NULL) {
-        return EAGAIN;  // Not enough memory to create the stack
+        return EAGAIN; 
     }
 
-    int flags = CLONE_SIGHAND | CLONE_VM | CLONE_FS | CLONE_FILES;  // We want the new thread to send a SIGCHLD signal when it exits
+    int flags = CLONE_SIGHAND | CLONE_VM | CLONE_FS | CLONE_FILES; 
 
     int (*clone_func)(void *) = (int (*)(void *))start_routine;
 
     pid_t pid = clone(clone_func, stack + STACK_SIZE, flags, arg);
 
     if (pid == -1) {
-        free(stack);  // Free the allocated memory if `clone` failed
-        return EAGAIN;  // Error creating the thread
+        free(stack); 
+        return EAGAIN;
     }
 
     *thread = pid;
 
-    return 0;  // Successfully created the thread
+    return 0;
 }
 
 void *thread_func(void *arg) {
